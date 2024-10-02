1a4a3e623cd90a1d675875563b7bec38b41899ef
S-K-Y-Light <s84915656@gmail.com>
2024-10-02 14:53:46 +0800

    Parse User Input for RemarkCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -61,7 +61,7 @@ public class AddressBookParser {
             return new EditCommandParser().parse(arguments);
 
         case RemarkCommand.COMMAND_WORD:
-            return new RemarkCommand();
+            return new RemarkCommandParser().parse(arguments);
 
         case DeleteCommand.COMMAND_WORD:
             return new DeleteCommandParser().parse(arguments);
